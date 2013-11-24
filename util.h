f62b8ee809fe3fb64db49dda7e1ceb3e398f55c3
katadh <katadh@sbcglobal.net>
2013-11-24 17:08:38 -0500

    finished functions for creating and opening packets, moved encryption and decryption stuff to separate functions

--- util.h
+++ util.h
@@ -1,12 +1,18 @@
 #ifndef __util_h__
 #define __util_h__
 
+#include <vector>
 #include <string>
 using std::string;
 
-string createPacket(string &nonce, string &message, string &newNonce);
 string makeNonce();
-string pad(std::string &packet);
-string unPad(std::string &packet);
+string createPacket(string &key, string &nonce, string &message, string &newNonce);
+std::vector<string> openPacket(string &packet, string &key);
+string generateAESKey();
+string generateAESIV();
+string encryptAES(string &plain, string &hexKey, string &hexIV);
+string decryptAES(string &cipher, string &hexKey, string &hexIV);
+string pad(string &packet);
+string unPad(string &packet);
 
 #endif
